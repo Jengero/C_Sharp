2077c2f99ef69daad1a5066a3cd17636995d7117
Egor Egorov <e.egorov.ordinary@gmail.com>
2023-06-05 00:54:37 +0300

    Finish homework #2

--- c_sharp_basics_1/Program.cs
+++ c_sharp_basics_1/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Security.Cryptography;
 using System.Text;
 using static System.Net.WebRequestMethods;
 
@@ -12,15 +14,15 @@ Console.WriteLine("Hello, Work!");
 //new Homework().Task_6();
 //new Homework().Extra_1();
 
-new Homework().Task_2_1();
-//new Homework().Task_2_2();
-//new Homework().Task_2_3();
-//new Homework().Task_2_4();
-//new Homework().Task_2_5();
-//new Homework().Task_2_6();
-//new Homework().Task_2_7();
-//new Homework().Task_2_8();
-//new Homework().Task_2_9();
+//new Homework_2().Task_2_1();
+//new Homework_2().Task_2_2();
+//new Homework_2().Task_2_3();
+//new Homework_2().Task_2_4();
+//new Homework_2().Task_2_5();
+//new Homework_2().Task_2_6();
+//new Homework_2().Task_2_7();
+//new Homework_2().Task_2_8();
+//new Homework_2().Task_2_9();
 
 class Homework
 {
@@ -131,55 +133,199 @@ class Homework_2
 {
     public void Task_2_1()
     {
-        Console.WriteLine("Enter fout tipes of data:");
-
-        int age = System.Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(age);
-        //double CorrectAnswers = Console.ReadLine();
-        //char grade = Console.ReadLine();
-        //bool ispasssed = Console.ReadLine();
-
+        Console.WriteLine("Enter four tipes of data:");
+        Console.WriteLine("int tipe:");
+        int int_ex = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("double tipe:");
+        double double_ex = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("float tipe:");
+        float float_ex = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("boolean tipe:");
+        bool bool_ex = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine($"Entered values: { int_ex}; {double_ex}; {float_ex}; {bool_ex}.");
     }
-
     public void Task_2_2()
     {
+        Console.WriteLine("Enter two tipes of data to convert:");
+        Console.WriteLine("Enter numeric type:");
+        var DoubleEx = Console.ReadLine();
+        Console.WriteLine("Enter boolean type:");
+        var BoolEx = Console.ReadLine();
+        Console.WriteLine("Result of the convertation:");
 
+        bool result1 = double.TryParse(DoubleEx, out double parsedDouble);
+        if (result1 == true) { Console.WriteLine(parsedDouble); }
+        else Console.WriteLine("First value has wrong data tipe!");
 
+        bool result2 = Boolean.TryParse(BoolEx, out bool parsedBool);
+        if (result2 == true) { Console.WriteLine(parsedBool); }
+        else Console.WriteLine("Second value has wrong data tipe!");
     }
     public void Task_2_3()
     {
-
-
+        Console.WriteLine("Enter numeric type of data:");
+        var Number = Console.ReadLine();
+        bool result = double.TryParse(Number, out double parsedNumber);
+        Console.WriteLine($"The conversion result - {result}");
+        Console.WriteLine(parsedNumber);
     }
     public void Task_2_4()
     {
-
-
+        Console.WriteLine(@"Initial value is 35. The second value is greater than the first by 3. 
+The third value is the result of summarise the first and the second.");
+        int a = 20;
+        int b = a + 3;
+        int c = a + b;
+        Console.WriteLine($"Result is {c}");
     }
     public void Task_2_5()
     {
+        // cheese, milk, beef, eggs, mushrooms
+        var product1 = "Cheese";
+        var product2 = "Milk";
+        var product3 = "Beef";
+        var product4 = "Eggs";
+        var product5 = "Mushrooms";
+
+        var mass1 = 0.25;
+        var mass2 = 1;
+        var mass3 = 0.965;
+        var mass4 = 1;
+        var mass5 = 0.800;
+
+        var cost1 = 22.20;
+        var cost2 = 2.80;
+        var cost3 = 29.99;
+        var cost4 = 3.64;
+        var cost5 = 8.90;
+
+        var TotalSum = (Math.Round((cost1 * mass1) + (cost2 * mass2) + (cost3 * mass3) + (cost4 * mass4) + (cost5 * mass5), 2));
+        var Sale = 0.15;
+        var SumOfSale = (Math.Round(TotalSum * Sale, 2));
+        var SumAfterSale = (Math.Round(TotalSum - Sale, 2));
+        
+        Console.WriteLine(@$"                  Payment check
+{product1}...{mass1} kg.......................{(Math.Round(cost1 * mass1, 2))} BYN
+{product2}...{mass2} l.......................{(Math.Round(cost2 * mass2, 2))} BYN
+{product3}...{mass3} kg.......................{(Math.Round(cost3 * mass3, 2))} BYN
+{product4}...{mass4} dozen.......................{(Math.Round(cost4 * mass4, 2))} BYN
+{product5}...{mass5} kg.......................{(Math.Round(cost5 * mass5, 2))} BYN
 
 
+Total cost.......................{TotalSum} BYN
+Sale.......................{SumOfSale} BYN
+
+Final cost.......................{SumAfterSale} BYN
+
+Thanks for your purchase!");
     }
     public void Task_2_6()
     {
+        double a = 11;
+        double b = 7;
+        double result1 = (a - b) * b / a + b;
+        // 1) a-b = 11 - 7 = 4 
+        // 2) 4 * b = 4 * 7 = 28
+        // 3) 28 / a = 28 / 11 ≈ 2.55
+        // 4) 2.55 + b = 2.55 + 7 ≈ 9.55
+        Console.WriteLine(Math.Round(result1, 2));
 
-
+        double result2 = ++result1 / (b+3);
+        Console.WriteLine(Math.Round(result2, 2));
+        // 1) ++result1 = 9.55 + 1 = 10.55
+        // 2) b+3 = 10
+        // 3) 10.55 / 10 ≈ 1.05
+        double result3 = result1-- * (b / 2) ;
+        double result4 = result1-- * (b / 2) ;
+        Console.WriteLine(Math.Round(result3, 2)); //???
+        Console.WriteLine(Math.Round(result4, 2)); 
+        // 1) b / 2 = 3.5
+        // 2) 9.55 * 3.5 ≈ 33.41
+        // 3) 9.55-- = 8.55
     }
     public void Task_2_7()
     {
+        int a1 = 5;
+        int b1 = 5;
 
+        double a2 = 6;
+        double b2 = 6;
+        var c2 = 6;
 
+        int a3 = 41;
+        int b3 = 31;
+        int c3 = 42;
+
+        int a4 = 324;
+        int b4 = 323;
+
+        int a5 = 65;
+        int b5 = 66;
+
+        int a1_1 = 6;
+        int b1_1 = 5;
+
+        double a2_1 = 7;
+        double b2_1 = 6;
+        var c2_1 = 6;
+
+        int a3_1 = 42;
+        int b3_1 = 42;
+        int c3_1 = 42;
+
+        int a4_1 = 323;
+        int b4_1 = 323;
+
+        int a5_1 = 67;
+        int b5_1 = 66;
+
+        // True sequence
+        bool r1 = a1 == b1;
+        Console.WriteLine(r1);
+
+        bool r2 = (a2 == c2) && (b2 == c2);
+        Console.WriteLine(r2);
+
+        bool r3 = (a3 != c3) || (b3 != c3);
+        Console.WriteLine(r3);
+
+        bool r4 = a4 != b4;
+        Console.WriteLine(r4);
+
+        bool r5 = a5 <= b5;
+        Console.WriteLine(r5);
+
+        // False sequence
+        bool res1 = a1_1 == b1_1;
+        Console.WriteLine(res1);
+
+        bool res2 = (a2_1 == c2_1) && (b2_1 == c2_1);
+        Console.WriteLine(res2);
+
+        bool res3 = (a3_1 != c3_1) || (b3_1 != c3_1);
+        Console.WriteLine(res3);
+
+        bool res4 = a4_1 != b4_1;
+        Console.WriteLine(res4);
+
+        bool res5 = a5_1 <= b5_1;
+        Console.WriteLine(res5);
     }
     public void Task_2_8()
     {
-
-
+        int ans = 42;
+        object temp = ans;
+        int wer = (int)ans;
     }
     public void Task_2_9()
     {
-
-
+        int a = 15;
+        //  неявное  преобразование
+        double b = a;
+    
+        Char c = '2';
+        // явное преобразование
+        int d = (int)c;
     }
 
 }
