cc4fca6d51308a35766926a2cf594e588f251587
Egor Egorov <e.egorov.ordinary@gmail.com>
2023-06-22 03:35:39 +0300

    Add homework #6

---
+++ CSharpOOP3/CSharpOOP3/LaptopHelper.cs
@@ -0,0 +1,25 @@
+﻿using CSharpOOP2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP3
+{
+    public static class LaptopHelper
+    {
+
+        public static void PrintScreenDiagonal(this Laptop laptop) 
+        {
+            Console.WriteLine($"This model has {laptop.ScreenDiagonal}\" screen diagonal ");
+        }
+
+
+        public static double CalculatePriceWithDiscount(this Laptop laptop)
+        {
+            laptop.Price = laptop.Price - (laptop.Price * Laptop.priceDiscount / 100);
+            return laptop.Price;
+        }
+    }
+}
---
+++ CSharpOOP3/CSharpOOP3/Program.cs
@@ -0,0 +1,31 @@
+﻿#region Task_1-2
+using CSharpOOP2;
+using CSharpOOP3;
+using System.Diagnostics;
+
+Laptop lenovo = new("Lenovo", 15.6, true, 7, true, 1800);
+
+lenovo.isExtendedWarranty = false;
+//Console.WriteLine($"{lenovo.IsExtendedWarranty}");              Error line
+//lenovo.LaptopVat = 0.1;                                         Error line
+Console.WriteLine($"Value added tax is {lenovo.LaptopVat}");
+#endregion
+#region Task_3
+Console.WriteLine($"{lenovo.Manufacturer}");
+#endregion
+#region Task_4
+Console.WriteLine($"Current CPU manufacturer is {Laptop.ProcessorManufacturer}");
+Laptop.ProcessorManufacturer = "Intel";
+Console.WriteLine($"New CPU manufacturer is {Laptop.ProcessorManufacturer}");
+#endregion
+#region Task_5
+Laptop.ChangePaymentMethod();
+#endregion
+#region Task_6-7
+lenovo.PrintScreenDiagonal();
+Console.WriteLine("Enter discount, %:");
+Laptop.priceDiscount = Laptop.ReadIntFromConsole();
+Console.WriteLine($"The price before discount is {lenovo.Price}");
+lenovo.CalculatePriceWithDiscount();
+Console.WriteLine($"The new price is {lenovo.Price}");
+#endregion
