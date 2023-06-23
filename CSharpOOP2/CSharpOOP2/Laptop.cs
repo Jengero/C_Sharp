19267a6561342e8b8c3816bfc1486b3ce31e3deb
Egor Egorov <108582432+Jengero@users.noreply.github.com>
2023-06-23 22:03:54 +0300

    Add homework #5 (#5)
    Co-authored-by: Egor Egorov <e.egorov.ordinary@gmail.com>

---
+++ CSharpOOP2/CSharpOOP2/Laptop.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP2
+{
+
+    public class Laptop
+    {
+        public string Manufacturer;
+        public double ScreenDiagonal;
+        public bool IsGraphicsCardDiscrete;
+        public int ProcessorGeneration = 3;
+
+        public Laptop(string Manufacturer, double ScreenDiagonal, bool IsGraphicsCardDiscrete, int ProcessorGeneration) {}
+        public Laptop() {}
+        public Laptop(string Manufacturer, double ScreenDiagonal) {}
+
+        public void ChangeScreenDiagonal()
+        {
+            Console.WriteLine($"Current diagonal of the screen is {ScreenDiagonal}");
+            Console.WriteLine("Select from the list below to what value to change the screen diagonal:");
+            Console.WriteLine("10.6\", 11.1\", 11.6\", 12.1\", 13.1\", 13.3\", 14.0\", 14.1\", 14.5\"");
+            Console.WriteLine("15.0\", 15.1\", 15.6\", 16.0\", 16.4\", 17.0\", 17.1\", 17.3\", 18.4\"");
+            double screenSize = ReadDoubleFromConsole();
+            double[] screenDiagonale = new double[] { 10.6, 11.1, 11.6, 12.1, 13.1, 13.3, 14.0, 14.1, 14.5, 15.0, 15.1, 15.6, 16.0, 16.4, 17.0, 17.1, 17.3, 18.4 };
+
+            for (int i = 0; i < screenDiagonale.Length; i++)
+            {
+                if (screenSize == screenDiagonale[i])
+                {
+                    ScreenDiagonal = screenSize;
+                    Console.WriteLine($"Screen size changed to {ScreenDiagonal}\"");
+                    break;
+                }
+                else if (screenSize != screenDiagonale[i] && i == 17)
+                {
+                    Console.WriteLine("Wrong value. Please select from the presented list");
+                    i = 0;
+                    screenSize = ReadDoubleFromConsole();
+                }
+                else { }
+            }
+        }
+        private void PrintScreenDiagonal() 
+        {
+            Console.WriteLine($"Current screen diagonal is {ScreenDiagonal}"); 
+        }
+        internal void PrintProcessorGeneration()
+        {
+            Console.WriteLine($"Current processor generation is {ProcessorGeneration}");
+        }
+
+        public double ReadDoubleFromConsole()
+        {
+            string enteredValue = Console.ReadLine();
+            enteredValue = enteredValue.Replace(',', '.');
+            double value;
+            bool check = true;
+            while (check != double.TryParse(enteredValue, out value))
+            {
+                Console.WriteLine("Wrong value. Try again");
+                enteredValue = Console.ReadLine();
+            }
+            return value;
+        }
+    }
+    
+}
---
+++ CSharpOOP2/CSharpOOP2/Program.cs
@@ -0,0 +1,28 @@
+﻿using CSharpOOP2;
+
+Laptop Asus = new();                    
+Asus.Manufacturer = "Asus";
+Asus.ScreenDiagonal = 16.0;
+Asus.IsGraphicsCardDiscrete = true;
+Asus.ProcessorGeneration = 9;
+
+Laptop Lenovo = new()
+{
+    Manufacturer = "Lenovo",
+    ScreenDiagonal = 15.6,
+    IsGraphicsCardDiscrete = true,
+    ProcessorGeneration = 7
+};
+
+Laptop HewlettPackard = new("HP", 17.1, false, 5); 
+
+Asus.ChangeScreenDiagonal();
+//Asus.PrintGraphicsCard(); Error Line. Private method can be used only within their own class. 
+Asus.PrintProcessorGeneration();
+
+Laptop Acer = new() { Manufacturer = "Acer", IsGraphicsCardDiscrete = true, ProcessorGeneration = 7 };
+Acer.ChangeScreenDiagonal(); // The default value of any double variable is 0. From the beginning of the method 0 is displayed in the console.
+
+Laptop MicroStarInternational = new() { Manufacturer = "MSI", ScreenDiagonal = 17.3, IsGraphicsCardDiscrete = true };  // Default value of ProcessorGeneration = 3
+Console.WriteLine($"Laptop {MicroStarInternational.Manufacturer} with {MicroStarInternational.ScreenDiagonal}\" " +
+    $"screen and processor Intel {MicroStarInternational.ProcessorGeneration}th generation ");
