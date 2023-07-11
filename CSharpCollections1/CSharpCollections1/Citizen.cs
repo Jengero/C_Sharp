fe0a517f984d4575e7b2290592dfad25dd594bf3
Egor Egorov <108582432+Jengero@users.noreply.github.com>
2023-07-12 00:56:21 +0300

    Add arrays' and collections' homework (#8)
    Co-authored-by: Egor Egorov <e.egorov.ordinary@gmail.com>

---
+++ CSharpCollections1/CSharpCollections1/Citizen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharpCollections1
+{
+    internal class Citizen
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
