1a75e50ca659b4347730bb7d4ad401fc7966934c
Egor Egorov <e.egorov.ordinary@gmail.com>
2023-07-17 23:32:09 +0300

    Add C# Advanced homework

---
+++ CSharpAdvanced/CSharpAdvanced/Rose.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpAdvanced
+{
+    public class Rose : Flower
+    {
+        private string _name;
+        private string _color;
+        private double _price;
+
+        public override string Name { get => _name; set => _name = value; }
+        public override string Color { get => _color; set => _color = value; }
+        public override double Price { get => _price; set => _price = value; }
+
+        public Rose(string name, string colour, double price) 
+        {
+            _name = name;
+            _color = colour;
+            _price = price;
+        }
+    }
+}
---
+++ CSharpAdvanced/CSharpAdvanced/Violet.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpAdvanced
+{
+    public class Violet : Flower
+    {
+        private string _name;
+        private string _color;
+        private double _price;
+
+        public override string Name { get => _name; set => _name = value; }
+        public override string Color { get => _color; set => _color = value; }
+        public override double Price { get => _price; set => _price = value; }
+
+        public Violet(string name, string colour, double price)
+        {
+            _name = name;
+            _color = colour;
+            _price = price;
+        }
+    }
+}
