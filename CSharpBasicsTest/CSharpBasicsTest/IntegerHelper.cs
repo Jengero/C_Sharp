4d846cb3234a8edbfdd192effe2c8df9e6a214b8
Egor Egorov <108582432+Jengero@users.noreply.github.com>
2023-08-03 00:12:03 +0300

    CSharp test basics
    Co-authored-by: Egor Egorov <e.egorov.ordinary@gmail.com>

---
+++ CSharpBasicsTest/CSharpBasicsTest/IntegerHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicsTest
+{
+    public static class IntegerHelper
+    {
+       public static int GetSquareInt( this int integer) 
+        {
+            int result = integer * integer;
+            return result;
+        }
+    }
+}
