24f977417a721adfb93ca446c5b71139e2d48c0d
Egor Egorov <108582432+Jengero@users.noreply.github.com>
2023-08-03 00:10:53 +0300

    Add patterns homework (#11)

---
+++ CSharpPatterns/CSharpPatterns/Decorator/ExtraLoudGigantosaurusDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPatterns.Decorator
+{
+    // Additional functionality that will be added to the main class and "wrap" it,
+    // thereby implementing the open-closed principle (Concrete Decorator)
+    public class ExtraLoudGigantosaurusDecorator : IGigantosaurus
+    {
+        private Gigantosaurus _gigantosaurus;
+        private string _extraLoudRoar;
+
+        public ExtraLoudGigantosaurusDecorator(Gigantosaurus gigantosaurus, string extaLoudRoar)
+        {
+            _gigantosaurus = gigantosaurus;
+            _extraLoudRoar = extaLoudRoar;
+        }
+
+        public string Roar()
+        {
+            return _gigantosaurus.Roar() + _extraLoudRoar;
+        }
+    }
+}
---
+++ CSharpPatterns/CSharpPatterns/Factory/Truck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPatterns.Factory
+{
+    // Implementation of the base interface whose object will create the factory method
+    public class Truck : IVehicle
+    {
+        public void PrintInformation()
+        {
+            Console.WriteLine("Truck has been created");
+        }
+    }
+}
