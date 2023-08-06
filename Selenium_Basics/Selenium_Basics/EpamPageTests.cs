50599d9343a235e85f14265fe3dbe15b5c2dbdbb
Egor Egorov <e.egorov.ordinary@gmail.com>
2023-08-07 01:16:37 +0300

    Fix naming, add error messages

--- Selenium_Basics/Selenium_Basics/EpamPageTests.cs
+++ Selenium_Basics/Selenium_Basics/EpamPageTests.cs
@@ -1,7 +1,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace NunitTestPilot
+namespace SeleniumBasics
 {
     public class EpamPageTests
     {
@@ -20,25 +20,25 @@ namespace NunitTestPilot
             var epamMainPage = "https://www.epam.com/";
 
             _driver.Navigate().GoToUrl(epamMainPage);
-            var currentOpenedUrl = _driver.Url.ToString();
+            var currentOpenedUrl = _driver.Url;
 
-            Assert.That(currentOpenedUrl.Equals(epamMainPage));
+            Assert.That(currentOpenedUrl.Equals(epamMainPage), "Page loaded incorrect");
         }
 
         [Test]
         public void CheckEpamPageTransitionTest()
         {
-            var epamHowWeDoItPage = "https://www.epam.com/how-we-do-it";
-            var epamOurWorkPage = "https://www.epam.com/our-work";
+            var epamHowWeDoItUrl = "https://www.epam.com/how-we-do-it";
+            var epamOurWorkUrl = "https://www.epam.com/our-work";
 
-            _driver.Navigate().GoToUrl(epamHowWeDoItPage);
-            _driver.Navigate().GoToUrl(epamOurWorkPage);
+            _driver.Navigate().GoToUrl(epamHowWeDoItUrl);
+            _driver.Navigate().GoToUrl(epamOurWorkUrl);
             _driver.Navigate().Refresh();
             _driver.Navigate().Back();
 
             var currentOpeedUrl = _driver.Url.ToString();
 
-            Assert.That(currentOpeedUrl.Equals(epamHowWeDoItPage));
+            Assert.That(currentOpeedUrl.Equals(epamHowWeDoItUrl), "Pages don't matches");
         }
 
         [TearDown]
